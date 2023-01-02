0c58fc20265f2506030f4f3cdaa814ca751248b3
Alexis Ngaing <ksankirara92@gmail.com>
2023-01-02 21:46:03 +0700

    usual commit

---
+++ Simple Game/source.js
@@ -0,0 +1,15 @@
+// Player choice
+var p = prompt('Rock, Paper, Scissors');
+
+// Computer choice
+// Random number
+var comp = Math.random();
+
+if (comp < 0.34) {
+    comp = 'Rock';
+} else if (comp >= 0.34) {
+
+}
+// Rules
+
+// Result
