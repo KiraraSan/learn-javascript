ad5be8aac5cd79161a8e40de4bf8e22c3e2af43e
Alexis Ngaing <ksankirara92@gmail.com>
2023-01-02 22:28:50 +0700

    usual commit

--- Simple Game/source.js
+++ Simple Game/source.js
@@ -1,15 +1,47 @@
-// Player choice
-var p = prompt('Rock, Paper, Scissors');
+var loop = true;
+while (loop) {
+    // Player choice
+    var p = prompt('Rock, Paper, Scissors');
 
-// Computer choice
-// Random number
-var comp = Math.random();
+    // Computer choice
+    // Random number
+    var comp = Math.random();
 
-if (comp < 0.34) {
-    comp = 'Rock';
-} else if (comp >= 0.34) {
+    if (comp < 0.34) {
+        comp = 'Rock';
+    } else if (comp >= 0.34 && comp < 0.67) {
+        comp = 'Paper';
+    } else {
+        comp = 'Scissors'
+    }
 
+    console.log(comp);
+
+    // Rules
+    var result = '';
+
+    if (p == comp) {
+        result = 'Draw';
+    } else if (p == 'Rock') {
+        // if (comp == 'Paper') {
+        //     result = 'Lose';
+        // } else {
+        //     result = 'Win';
+        // }
+        result = (comp == 'Paper') ? 'Lose' : 'Win';
+    } else if (p == 'Paper') {
+        result = (comp == 'Rock') ? 'Win' : 'Lose';
+    } else if (p == 'Scissors') {
+        result = (comp == 'Paper') ? 'Win' : 'Lose';
+    } else {
+        result = 'Invalid input [!]';
+    }
+
+    // Result
+    alert('You : ' + p + '\nComp : ' + comp + '\n\nResult : ' + result);
+
+    // Loop
+    loop = confirm('Again?');
 }
-// Rules
 
-// Result
+alert('Thank You!');
