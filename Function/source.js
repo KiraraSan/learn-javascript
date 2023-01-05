90f468c607e3182332193b0e30f5f2a0f97e671c
Alexis Ngaing <ksankirara92@gmail.com>
2023-01-05 10:31:32 +0700

    usual commit

--- Function/source.js
+++ Function/source.js
@@ -33,17 +33,49 @@
 // alert(sum_two_number(10, 20));
 
 // Find cube area
-function findCubeArea(a, b) {
-    var volumeA;
-    var volumeB;
-    var total;
+// function findCubeArea(a, b) {
+//     var volumeA;
+//     var volumeB;
+//     var total;
 
-    volumeA = a * a * a;
-    volumeB = b * b * b;
+//     volumeA = a * a * a;
+//     volumeB = b * b * b;
 
-    total = volumeA + volumeB;
+//     total = volumeA + volumeB;
     
+//     return total;
+// }
+
+// console.log(findCubeArea(10, 5));
+
+// function add(a, b) {
+//      return a + b;
+// }
+
+// function multiplication(a, b) {
+//     return a * b;
+// }
+
+// // var a = parseInt(prompt('Input number a : '));
+// // var b = parseInt(prompt('Input number b : '));
+// // var total = add(a * 2, b * 2);
+
+// var total = multiplication(add(2, 4), add(5, 6));
+
+// console.log(total);
+
+// function retw(a, b, c, d, e);
+
+// retw(5, 10, 15, 'Hello', false); 
+
+function add() {
+    var total = 0;
+    
+    for (var i = 0; i < arguments.length; i++) {
+        total += arguments[i];
+    }
     return total;
 }
 
-console.log(findCubeArea(10, 5));
+var plus = add(1, 2, 3, 4);
+console.log(plus);
