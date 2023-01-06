9e42ddb58bf980ad72f505b7ebe24713a94c5338
Alexis Ngaing <ksankirara92@gmail.com>
2023-01-06 11:53:31 +0700

    update

---
+++ Array/script.js
@@ -0,0 +1,55 @@
+// var animal = [];
+// animal = ['Cat', 'Dog', 'Panda', 'Fox', 'Cow'];
+
+// console.log(animal[4]);
+// console.log(animal.length);
+
+// var arr = ['String', 1, true];
+
+// var func = function() {
+//     alert('Hello, World!');
+// }
+
+// var arr2 = ['String', 1, true, func];
+
+// // Multidimention array
+// var arr3 = ['String', 1, true, func, [2, 4, 6]];
+// console.log(arr3[4][1]);
+
+// =================================================
+// // Input array
+// var array = [];
+// array[0] = 'Alex';
+// array[1] = 19;
+// array[2] = true;
+
+// console.log(array);
+
+// // Delete array
+// var array = ['Nama'];
+// array[0] = undefined;
+// console.log(array);
+
+// // Print array
+// var array = ['String', 'AnotherString', 'StringString'];
+// for (var i = 0; i < array.length; i++) {
+//     console.log('Name in index ' + i + ' : ' + array[i]);
+// }
+
+// =============================================================
+// Method
+// lenght, join, push, pop, shitf, unshift, slice, splice
+
+// Join
+var array = ['Name1', 'Name2', 'Name3'];
+// console.log(array.join(' - '));n
+
+// Push & Pop // Input and delete from tail
+// array.push('Name4', 'Name 5');
+// array.pop();
+// console.log(array.join(' - '));
+
+// Unshift & Shift // Input and delete from head
+// array.unshift('Name6');
+// array.shift();
+console.log(array.join(' - '));
